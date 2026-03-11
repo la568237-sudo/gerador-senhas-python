430d9e43b8ddfb26895c5ae5c98c04049ffe2c2a
Leandro Castro <la568237@gmail.com>
2026-03-11 17:25:15 -0300

    Add password generator function in gerador_senha.py

--- gerador_senha.py
+++ gerador_senha.py
@@ -1,1 +1,25 @@
+import random
+import string
 
+def gerar_senha(tamanho):
+    letras = string.ascii_letters
+    numeros = string.digits
+    simbolos = string.punctuation
+
+    caracteres = letras + numeros + simbolos
+
+    senha = ""
+
+    for i in range(tamanho):
+        senha += random.choice(caracteres)
+
+    return senha
+
+
+print("Gerador de Senhas Seguras")
+
+tamanho = int(input("Digite o tamanho da senha: "))
+
+senha = gerar_senha(tamanho)
+
+print("Senha gerada:", senha)
